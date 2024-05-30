733126309296713f2e30535c28bcd90602d1f2f1
Zabero <s.zaborski@gmail.com>
2024-05-30 11:31:04 +0200

    wywalenie borderów z przycisków

--- src/ChatClientGUI.java
+++ src/ChatClientGUI.java
@@ -27,7 +27,7 @@ public class ChatClientGUI {
 
         // Okno rozmowy
         chatWindow = new JTextArea();
-        chatWindow.setBorder(BorderFactory.createLineBorder(Color.ORANGE, 2));
+        chatWindow.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         chatWindow.setBounds(10, 10, 550, 400); // Ustal pozycję i rozmiar zgodnie ze schematem
         chatWindow.setEditable(false); // make chat window read-only
         mainPanel.add(chatWindow);
@@ -35,37 +35,37 @@ public class ChatClientGUI {
         // Lista podłączonych użytkowników
         userListModel = new DefaultListModel<>();
         userList = new JList<>(userListModel);
-        userList.setBorder(BorderFactory.createLineBorder(Color.CYAN, 2));
+        userList.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         userList.setBounds(570, 10, 200, 200); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(userList);
 
         // Okno wpisywania wiadomości
         messageInput = new JTextArea();
-        messageInput.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
+        messageInput.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         messageInput.setBounds(10, 420, 550, 100); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(messageInput);
 
         // Przycisk wyślij wiadomość
         JButton sendMessageButton = new JButton("Przycisk wyślij wiadomość");
-        sendMessageButton.setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 2));
+        //sendMessageButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         sendMessageButton.setBounds(10, 530, 150, 30); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(sendMessageButton);
 
         // Przycisk dodaj osobę do listy kontaktów
         JButton addUserButton = new JButton("Przycisk doda osobę do listy kontaktów");
-        addUserButton.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 2));
+        //addUserButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         addUserButton.setBounds(570, 220, 200, 30); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(addUserButton);
 
         // Przycisk usuń z kontaktów daną osobę
         JButton removeUserButton = new JButton("Przycisk usuń z kontaktów daną osobę");
-        removeUserButton.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 2));
+        //removeUserButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         removeUserButton.setBounds(570, 260, 200, 30); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(removeUserButton);
 
         // Pokaż historię rozmowy
         JButton showHistoryButton = new JButton("Pokaż historię rozmowy");
-        showHistoryButton.setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 2));
+        //showHistoryButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         showHistoryButton.setBounds(570, 300, 200, 30); // Ustal pozycję i rozmiar zgodnie ze schematem
         mainPanel.add(showHistoryButton);
 
